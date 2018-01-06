a443a33f5d0d89fcf41d4cd583b662ce0866d7a8
FukushimaTakeshi <whippet.818@gmail.com>
2018-01-06 20:45:58 +0900

    fix typo

--- lib/ruboty/handlers/time_clock.rb
+++ lib/ruboty/handlers/time_clock.rb
@@ -3,7 +3,7 @@ require "ruboty/time_clock/actions/time_clock"
 module Ruboty
   module Handlers
     class TimeClock < Base
-      on /time_clock (?<number>.*?)\z/, name: 'time_clock', description: 'output time_clock result'
+      on /time_clock\z/i, name: 'time_clock', description: 'output time_clock result'
     end
 
     def time_clock(message)
